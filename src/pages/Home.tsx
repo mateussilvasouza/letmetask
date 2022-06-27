3f21426a6141fce3cffbd26dcc9ea5d126e18ad4
Mateus TI Vai Fácil <mateustivaifcil@pop-os.localdomain>
2022-06-27 14:38:31 -0300

    Inicio do desenvolvimento da Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIcon from '../assets/images/google-icon.svg'
 
 export const Home = ()=>{
     return(
@@ -8,6 +10,25 @@ export const Home = ()=>{
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo real.</p>
             </aside>
+            <main>
+                <div>
+                    <img src={logoImg} alt="Logo Let Me Ask" />
+                    <button>
+                        <img src={googleIcon} alt="Icone do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>ou entre em uma sala</div>
+                    <form action="">
+                        <input 
+                            type="text"
+                            placeholder='Digite o código da sala'    
+                        />
+                        <button type="submit">
+                            Entrar na Sala
+                        </button>
+                    </form>
+                </div>
+            </main>
         </div>
     )
 }
