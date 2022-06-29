f50ad404c3ecf1546db1e064eb3f93962bc4ce72
Mateus Silva Souza <mateussilvasouza@pop-os.localdomain>
2022-06-28 22:31:22 -0300

    Criando GlobalStyle

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,17 @@
+import createGlobalStyle from 'styled-components'
+
+const GlobalStyle = createGlobalStyle<T>`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        font-size: 16px;
+        font-family: Roboto, Poppins, sans-serif
+    }
+    
+    
+`
+export default GlobalStyle;
